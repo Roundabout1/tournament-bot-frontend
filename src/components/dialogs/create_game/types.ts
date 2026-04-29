1b222201758628cf15bc8757eb9b4e26ee3f079e
Roundabout1 <mrsivannap@gmail.com>
2026-04-29 13:59:26 +0800

    prettier

--- src/components/dialogs/create_game/types.ts
+++ src/components/dialogs/create_game/types.ts
@@ -36,14 +36,15 @@ export interface FinesData {
 export interface ConfirmData {
   confirm: boolean;
 }
-export type CreateGameStep = 'entry_players_count' |
-  'entry_shuffle_type' |
-  'set_multi_tour_group_size' |
-  'set_multiplier' |
-  'entry_tours_count' |
-  'entry_is_asymmetric' |
-  'entry_has_game_fine' |
-  'create_game_finish' |
-  'confirm_new_game' |
-  'entry_error' |
-  'cancel_game_creation';
+export type CreateGameStep =
+  | 'entry_players_count'
+  | 'entry_shuffle_type'
+  | 'set_multi_tour_group_size'
+  | 'set_multiplier'
+  | 'entry_tours_count'
+  | 'entry_is_asymmetric'
+  | 'entry_has_game_fine'
+  | 'create_game_finish'
+  | 'confirm_new_game'
+  | 'entry_error'
+  | 'cancel_game_creation';
