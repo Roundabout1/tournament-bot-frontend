ca768b82207ab9ae80cbfb62fba2a0cf10373e75
Roundabout1 <mrsivannap@gmail.com>
2026-06-05 20:38:39 +0800

    Логин и пароль

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { Control } from './Control';
 import { Observer } from './Observer';
 import { ModeSelect } from './ModeSelect';
 import { AppMode } from '../types/Modes';
+import { Auth } from './Auth';
 
 export const App: React.FC = () => {
   const [mode, setMode] = useState<AppMode>('select');
@@ -20,13 +21,20 @@ export const App: React.FC = () => {
   };
 
   if (mode === 'admin') {
-    return <Control isAdmin={true} />;
+    return (
+      <Auth>
+        <Control isAdmin={true} />
+      </Auth>
+    );
   }
 
   if (mode === 'judge') {
-    return <Control isAdmin={false} />;
+    return (
+      <Auth>
+        <Control isAdmin={false} />
+      </Auth>
+    );
   }
-
 
   if (mode === 'observer') {
     return <Observer />;
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,77 +1,33 @@
-import { useState } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { Input } from './Input';
 import { Title } from './Title';
-import { useAuthContext } from './context/AuthContext';
 import { twMerge } from 'tailwind-merge';
-import { useNavigate } from 'react-router';
 
-export const Auth: React.FC = () => {
-  const context = useAuthContext();
-  const navigate = useNavigate();
-  const [error, setError] = useState<string | undefined>(undefined);
+export const Auth: React.FC<PropsWithChildren> = ({ children }) => {
+  const [error] = useState<string | undefined>(undefined);
   const [login, setLogin] = useState<string | undefined>(undefined);
   const [password, setPassword] = useState<string | undefined>(undefined);
   const [isWaitingData, setIsWaitingData] = useState<boolean>(false);
+  const [successfulSubmit, setSuccessfulSubmit] = useState<boolean>(false);
 
-  const clear = () => {
-    setError(undefined);
-    setLogin(undefined);
-    setPassword(undefined);
-    setIsWaitingData(false);
-  };
+  useEffect(() => {
+    const savedLogin = sessionStorage.getItem('tournament_bot_login') as string;
+    setLogin(savedLogin);
+    const savedPassword = sessionStorage.getItem('tournament_bot_password') as string;
+    setPassword(savedPassword);
+  }, []);
 
   const handleSubmit = async (e: any) => {
-    if (!context) {
-      throw new Error('Ошибка! Отсутствует контекст!');
-    }
     e.preventDefault();
     e.target.reset();
 
     if (!password || !login) return;
 
-    const [, serCurrentUser] = context;
-
     setIsWaitingData(true);
 
-    const formData = new FormData();
-
-    formData.append('password', password);
-    formData.append('username', login);
-    try {
-      const response = await fetch(`api/token`, {
-        mode: 'cors',
-        method: 'POST',
-        headers: { 'Access-Control-Allow-Origin': '*', accept: 'application/json' },
-        body: formData,
-      });
-      if (!response.ok) {
-        setIsWaitingData(false);
-        return;
-      }
-      const token = (await response.json())['access_token'];
-      const adminResponse = await fetch(`api/token`, {
-        mode: 'cors',
-        method: 'POST',
-        headers: { 'Access-Control-Allow-Origin': '*', accept: 'application/json' },
-        body: formData,
-      }).then(async (rawResponse) => await rawResponse.json());
-
-      const isAdmin = adminResponse['is_admin'];
-      serCurrentUser({
-        token: token,
-        isAdmin: isAdmin,
-      });
-
-      sessionStorage.clear();
-      if (response.ok) {
-        sessionStorage.setItem('isAdmin', isAdmin);
-        sessionStorage.setItem('token', token);
-        navigate('/');
-      }
-    } finally {
-      clear();
-      setError('Неверный логин или пароль!');
-    }
+    // TODO: послать ws-сообщение с логином и паролём
+    console.log(`${password} | ${login}`);
+    setSuccessfulSubmit(true);
   };
   const onLoginChange = (value: string) => {
     setLogin(value);
@@ -79,6 +35,9 @@ export const Auth: React.FC = () => {
   const onPasswordChange = (value: string) => {
     setPassword(value);
   };
+  if (successfulSubmit) {
+    return <div>{children}</div>;
+  }
   return (
     <div className="flex h-[100vh] w-[100wh] place-content-center items-center bg-gray-700">
       <div className="mb-[5%] flex-col items-center text-center">
