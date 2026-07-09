b694b4c64729d10618717a4fd894503b5e4f4c62
Roundabout1 <mrsivannap@gmail.com>
2026-07-09 14:09:26 +0800

    Константы для SessionStorage

---
+++ src/consts/StorageKeys.ts
@@ -0,0 +1,4 @@
+export const MESSAGES_STORAGE_KEY = 'tournament_chat_messages';
+export const LOGIN_STORAGE_KEY = 'tournament_bot_login';
+export const PASS_STORAGE_KEY = 'tournament_bot_password';
+export const MODE_STORAGE_KEY = 'tournament_app_mode';
