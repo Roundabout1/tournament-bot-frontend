511989dd71f47a8f6c12f57747ab4c75a7ee2045
Roundabout1 <mrsivannap@gmail.com>
2026-07-13 18:05:47 +0800

    Удаление старого кода

--- src/components/dialogs/create_game/types.ts
+++ src/components/dialogs/create_game/types.ts
@@ -1,48 +1,12 @@
-export interface StepProps {
-  onNext: (data?: any) => void;
-  onCancel: () => void;
-  onBack?: () => void;
-  data?: any;
-}
-
-export interface PlayersCountData {
-  count: number;
-}
-
-export interface ShuffleTypeData {
-  shuffle: string;
-}
-
-export interface GroupSizeData {
-  count: number;
-}
-
-export interface MultiplierData {
-  count: number;
-}
-
-export interface ToursCountData {
-  count: number;
-}
-
-export interface AsymmetricData {
-  is_asymmetric: boolean;
-}
-
-export interface FinesData {
-  has_fines: boolean;
-}
-
 export interface ConfirmData {
   confirm: boolean;
 }
 
 export enum ShuffleType {
-  Round = "Круговая",
-  Rating = "Рейтинговая",
-  Random = "Случайная",
-  MultiTournament = "Мульти-турнир",
+  Round = 'Круговая',
+  Rating = 'Рейтинговая',
+  Random = 'Случайная',
+  MultiTournament = 'Мульти-турнир',
 }
 
 export type CreateGameState = 'start' | 'finish' | 'cancel' | 'confirm';
-
